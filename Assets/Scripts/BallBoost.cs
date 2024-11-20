491e98e6639046ff61d1689523583547447fd9a8
MICHAL-PC\Admin <brozda.it@gmail.com>
2024-11-20 20:02:49 +0100

    added boost option to ball

---
+++ Assets/Scripts/BallBoost.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UIElements;
+[RequireComponent(typeof(Rigidbody))]
+
+public class BallBoost : MonoBehaviour
+{
+
+    [SerializeField]
+    float boostForce = 1f;
+
+    GameObject boostText;
+    Rigidbody rb;
+    bool boostActivated = false;
+    bool boostReady = true;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        boostText = GameObject.Find("Boost Text").gameObject;
+        TextVisible();
+    }
+
+    void Update()
+    {
+        Debug.Log(rb.velocity.normalized);
+        TextVisible();
+
+        if (Input.GetKeyDown(KeyCode.Space) && boostReady && rb.velocity != Vector3.zero)
+        {
+            boostActivated = true;
+            boostReady = false;
+            StartCoroutine("BoostCoroutine");
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (boostActivated)
+        {
+            rb.AddForce(rb.velocity.normalized * boostForce, ForceMode.Impulse);
+            boostActivated = false;
+        }
+    }
+
+    IEnumerator BoostCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        boostReady = true;
+        yield break;
+    }
+       private void TextVisible()
+    {
+        if (boostReady)
+        {
+            boostText.SetActive(true);
+        }
+        else
+        {
+           boostText.SetActive(false);
+        }
+    }
+}
