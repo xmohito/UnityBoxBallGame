af4c12ea4a2a9d4f90ee55658c0cf09a5f4a6d97
MICHAL-PC\Admin <brozda.it@gmail.com>
2024-11-20 19:32:18 +0100

    adding move to boxes using raycast

---
+++ Assets/Scripts/MoveBox.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBox : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    public bool reverse = false;
+    Vector3 force;
+    void Start()
+    {
+        force = Vector3.left * speed;
+        
+        if (reverse)
+        {
+            ReverseVector();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.DrawLine(transform.position, direction, Color.red, 0.1f, false); 
+
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            if (hit.transform.gameObject.CompareTag("Floor") || hit.transform.gameObject.CompareTag("Player"))
+            {
+                Move();
+            }
+            else
+            {
+                ReverseVector();
+                Move();
+            }
+        }
+        else
+        {
+            ReverseVector();
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        transform.GetComponent<Rigidbody>().velocity = force;
+    }
+
+    void ReverseVector()
+    {
+       force = -force;
+       direction.Set(-direction.x, direction.y, direction.y);
+    }
+
+}
